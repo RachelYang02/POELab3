40bf8e39eae2c4917a3073ed63c42bd9a776c974
RachelYang02 <rachel.yang@students.olin.edu>
2016-10-13 21:35:34 -0400

    2.0

--- motor_unit_testing/motor_unit_testing.ino
+++ motor_unit_testing/motor_unit_testing.ino
@@ -3,25 +3,31 @@
 
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
-#include "utility/Adafruit_PWMServoDriver.h"
+#include <Adafruit_PWMServoDriver.h>
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 
 // request the DC motor from the motor shield
 // may need to change (1) to whatever port # the motor is connected to
 // M1 = 1, M2 = 2, M3 = 3, M4 = 4
-Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *myMotor_L = AFMS.getMotor(3);
+Adafruit_DCMotor *myMotor_R = AFMS.getMotor(4);
 
 void setup() {
   // starts motor shield
   AFMS.begin();
-  // sets default speed of the motor
-  // ranges from 0 (stopped) to 255 (full speed)
-  myMotor->setSpeed(100); 
 }
 
 void loop() {
+  // sets default speed of the motor
+  // ranges from 0 (stopped) to 255 (full speed)
+  myMotor_L->setSpeed(100);
+  myMotor_R->setSpeed(100); 
+
   // run(direction)
   // direction can be FORWARD, BACKWARD, or RELEASE
-  myMotor->run(FORWARD);
+  // as of right now, seems to be that FORWARD is actually, in our perspective, a backwards motion. Keep in mind.
+  myMotor_L->run(FORWARD);
+  myMotor_R->run(FORWARD);
+
 }
